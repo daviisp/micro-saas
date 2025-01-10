d9023d85e90c9399d17561eab3291ff9d16646f8
Davi Silva <davi.pimenta8@gmail.com>
2025-01-10 12:23:20 -0300

    fix: add environment variable to use in deploy

--- src/services/stripe/index.ts
+++ src/services/stripe/index.ts
@@ -55,14 +55,14 @@ export async function createCheckoutSession(
 
     const checkoutSession = await stripe.billingPortal.sessions.create({
       customer: stripeCustomerId,
-      return_url: "http://localhost:3000/app/settings/billing",
+      return_url: `${process.env.NEXT_PUBLIC_URL}/app/settings/billing`,
       flow_data: {
         type: "subscription_update_confirm",
         after_completion: {
           type: "redirect",
           redirect: {
             return_url:
-              "http://localhost:3000/app/settings/billing?success=true",
+            `${process.env.NEXT_PUBLIC_URL}/app/settings/billing=true`,
           },
         },
         subscription_update_confirm: {
